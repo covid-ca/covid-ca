53abdc37ccced42c87d7d5073f5661decdf0782c
Covid CA <covidcdn@gmail.com>
2020-04-04 11:59:55 -0400

    Add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,24 @@ import styled from 'styled-components';
 import { background } from './constants/color';
 import { getConfirmedData } from './data/JHU';
 import { ProvincialTrend } from './types/responses';
+import { GlobalStyles } from './styles/globals';
 
 const AppBackground = styled('div')`
   background-color: ${background};
   min-height: 100vh;
 `;
 
-function App() {
+export function App() {
   const [trends, setTrends] = useState<ProvincialTrend[]>([]);
 
   useEffect(() => {
     getConfirmedData().then(setTrends);
   }, []);
 
-  return <AppBackground>Data points: {trends.length}</AppBackground>;
+  return (
+    <>
+      <GlobalStyles />
+      <AppBackground>Data points: {trends.length}</AppBackground>
+    </>
+  );
 }
-
-export default App;
